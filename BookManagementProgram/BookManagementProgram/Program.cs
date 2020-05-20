e71ba93d628be8ddcbdc8bd88bec69e90f2a2c24
Lee Yun Hwan <zoo7930@naver.com>
2020-05-20 18:05:19 +0900

    개발 시작(이전 프로그램 불러옴)

---
+++ BookManagementProgram/BookManagementProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagementProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookManagementProgram bookManagementProgram = new BookManagementProgram();
+            bookManagementProgram.StartProgram();
+        }
+    }
+}
